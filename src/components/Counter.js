fb89f9417eb0ba0addb703b6dd2ae16372067ec8
tylerfurgalack <104029862+tylerfurgalack@users.noreply.github.com>
2022-06-11 14:53:06 -0400

    basic plus and minus component created called "Counter"

---
+++ src/components/Counter.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import './Counter.css';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <button className="plusMinusBtn" onClick={() => setCount(count - 1)}> - </button>
+      <h2 className="displayNum"> {count} </h2>
+      <button className="plusMinusBtn" onClick={() => setCount(count + 1)}> + </button>
+    </div>
+  );
+};
+
+export default Counter;
