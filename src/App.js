c1f3301a9c01029c8019889fe718e963b558a1a5
tylerfurgalack <104029862+tylerfurgalack@users.noreply.github.com>
2022-06-21 13:42:58 -0400

    restructured and downsized component tree, also scraping some styling. Changing idea from here

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
-import CounterBar from "./components/Bar/CounterBar";
+import KtLogo from "./KtLogo.jpg";
+import Counter from "./components/Counter";
+import "./components/CounterBar.css"
+
 
 function App() {
   return (
-    <CounterBar />
-  )
+    <div className="barContainer">
+      <div className="counterContainer">
+        <h2 className="displayText">Command Points</h2>
+        <Counter />
+      </div>
+      <div className="imageContainer">
+        <img src={KtLogo} alt="" />
+      </div>
+      <div className="counterContainer">
+        <h2 className="displayText">Victory Points</h2>
+        <Counter />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Body.js
+++
@@ -1,5 +0,0 @@
-const Body = () => {
-
-}
-
-export default Body;
