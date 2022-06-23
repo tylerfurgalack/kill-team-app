220cdc73b6b35276792e792acf86b7997f8330e5
tylerfurgalack <104029862+tylerfurgalack@users.noreply.github.com>
2022-06-22 20:14:19 -0400

    moved the input component into Counter.js and created seperate count state for input

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,42 @@
-import { useState } from "react";
-import './Counter.css';
+import { useState, useEffect } from "react";
+import "./Counter.css";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [inputCount, setInputCount] = useState(0);
+
+  // useEffect(() => {
+
+  // })
 
   return (
-    <div className="container">
-      <button className="plusMinusBtn" onClick={() => setCount(count - 1)}> - </button>
-      <h2 className="displayNum"> {count} </h2>
-      <button className="plusMinusBtn" onClick={() => setCount(count + 1)}> + </button>
+    <div>
+      <div className="container">
+        <button
+          id="minBtn"
+          className="plusMinusBtn"
+          onClick={() => {
+            setCount(count - 1);
+            setInputCount(inputCount - 1);
+          }}
+        >
+          -
+        </button>
+        <h2 className="displayNum"> {count} </h2>
+        <button
+          id="plusBtn"
+          className="plusMinusBtn"
+          onClick={() => {
+            setCount(count + 1);
+            setInputCount(inputCount + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
+      <label className="displayNum"> Points History</label>
+      <input type="text" value={inputCount} readOnly></input>
+      <button id="submitBtn" type="submit">Enter</button>
     </div>
   );
 };
